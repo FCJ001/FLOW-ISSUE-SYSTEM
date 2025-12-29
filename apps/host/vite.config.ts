1b91bf996e18527ef84c58e39f28f647f99423fc
chunjiu <1614949142@qq.com>
2025-12-29 20:09:15 +0800

    feat: 修改deploy.yml

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import federation from "@originjs/vite-plugin-federation";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
   plugins: [
     react(),
     federation({
       remotes: {
-        web: "http://localhost:5174/assets/remoteEntry.js",
+        web: 'http://localhost:5174/assets/remoteEntry.js',
       },
-      shared: ["react", "react-dom"],
+      shared: ['react', 'react-dom'],
     }),
   ],
+  base: '/FLOW-ISSUE-SYSTEM/',
   server: {
     port: 5173,
     fs: {
